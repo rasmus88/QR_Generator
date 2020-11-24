f5646e3857da01c803431bce0ab294b563375445
Rasmus <rasmusadolfsson@hotmail.com>
2020-11-24 16:06:20 +0100

    Added bootstrap & icons plugin

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  router, 
   render: h => h(App)
 }).$mount('#app')
+
+// Plugins 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
